e6d732dfb1a591859ee9090ab528d010ea040364
phelsajc <phelsajc@users.noreply.github.com>
2025-10-16 11:55:24 +0800

    Add stock_trans_iss_items

--- app/Http/Controllers/MigrationValidationController.php
+++ app/Http/Controllers/MigrationValidationController.php
@@ -166,6 +166,14 @@ class MigrationValidationController extends Controller
             'identifier_field' => '_id',
             'pipeline_type' => 'complex'
         ],
+        'stock_trans_iss_items' => [
+            'mongodb_collection' => 'stocktransfers',
+            'mssql_table' => 'stock_trans_iss_items',
+            'date_field_mongo' => 'modifiedat',
+            'date_field_mssql' => 'modifieddate',
+            'identifier_field' => '_id',
+            'pipeline_type' => 'complex'
+        ],
     ];
     /**
      * Display the migration validation dashboard
@@ -1807,6 +1815,82 @@ class MigrationValidationController extends Controller
                             'path' => '$orgDetails',
                             'preserveNullAndEmptyArrays' => true
                         ]
+                    ],
+                    [
+                        '$match' => [
+                            'modifiedat' => [
+                                '$gte' => $startISODate,
+                                '$lte' => $endISODate
+                            ]
+                        ]
+                    ],
+                    [
+                        '$count' => 'Total'
+                    ]
+                ];
+            case 'stock_trans_iss_items':
+                return [
+                    [
+                        '$unwind' => [
+                            'path' => '$itemdetails',
+                            'preserveNullAndEmptyArrays' => true,
+                        ],
+                    ],
+                    [
+                        '$lookup' => [
+                            'from' => 'itemmasters',
+                            'localField' => 'itemdetails.itemmasteruid',
+                            'foreignField' => '_id',
+                            'as' => 'imDetails',
+                        ],
+                    ],
+                    [
+                        '$unwind' => [
+                            'path' => '$imDetails',
+                            'preserveNullAndEmptyArrays' => true,
+                        ],
+                    ],
+                    [
+                        '$lookup' => [
+                            'from' => 'referencevalues',
+                            'localField' => 'imDetails.itemcategoryuid',
+                            'foreignField' => '_id',
+                            'as' => 'imCatDetails',
+                        ],
+                    ],
+                    [
+                        '$unwind' => [
+                            'path' => '$imCatDetails',
+                            'preserveNullAndEmptyArrays' => true,
+                        ],
+                    ],
+                    [
+                        '$lookup' => [
+                            'from' => 'organisations',
+                            'localField' => 'orguid',
+                            'foreignField' => '_id',
+                            'as' => 'orgDetails',
+                        ],
+                    ],
+                    [
+                        '$unwind' => [
+                            'path' => '$orgDetails',
+                            'preserveNullAndEmptyArrays' => true,
+                        ],
+                    ],
+                    [
+                        '$lookup' => [
+                            'from' => 'referencevalues',
+                            'localField' => 'itemdetails.statusuid',
+                            'foreignField' => '_id',
+                            'as' => 'statusDetails',
+                        ],
+                    ],
+                    [
+                        '$unwind' => [
+                            'path' => '$statusDetails',
+                            'preserveNullAndEmptyArrays' => true,
+                        ],
                     ],
                     [
                         '$match' => [
